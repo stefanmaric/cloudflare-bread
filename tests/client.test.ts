dcb78c4fca68a42583c48d75ba6d062d05d26e64
Stefan Maric <me@stefanmaric.com>
2023-07-02 13:43:09 +0200

    Add a few more tests for dynamic accessors

--- tests/client.test.ts
+++ tests/client.test.ts
@@ -1,4 +1,3 @@
-
 import { describe, it } from 'vitest'
 import { createClient } from '../src/client'
 
@@ -11,5 +10,34 @@ describe('Basic functionality', () => {
     const client = createClient({ token: 'SUPER_SECRET' })
     const account = client.accounts.accountIdentifier('FAKE_ACCOUNT')
     expect(account).toBeTruthy()
+  })
+})
+
+describe('Dynamic accessors', () => {
+  it.concurrent('It creates new wrappers that can be chained', async ({ expect }) => {
+    const client = createClient({ token: 'SUPER_SECRET' })
+    const user = client.user
+    expect(user).toBeTruthy()
+    const billing = user.billing
+    expect(billing).toBeTruthy()
+    const history = billing.history
+    expect(history).toBeTruthy()
+  })
+  it.concurrent('It creates new wrappers from parameterized segments', async ({ expect }) => {
+    const client = createClient({ token: 'SUPER_SECRET' })
+    const accounts = client.accounts.accountIdentifier('FAKE_ACCOUNT')
+    expect(accounts).toBeTruthy()
+    const billing = accounts.billing
+    expect(billing).toBeTruthy()
+    const profile = billing.profile
+    expect(profile).toBeTruthy()
+  })
+  it.concurrent('Accessors work with object destructing', async ({ expect }) => {
+    const client = createClient({ token: 'SUPER_SECRET' })
+    const workers = client.accounts.accountIdentifier('FAKE_ACCOUNT').workers
+    const { domains, scripts, deployments } = workers
+    expect(domains).toBeTruthy()
+    expect(scripts).toBeTruthy()
+    expect(deployments).toBeTruthy()
   })
 })
